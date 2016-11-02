f8b4026f0ff8612c48537ed6510842540213a8ed
sean <zhxiang.guan@gmail.com>
2016-11-03 00:18:10 +0800

    Create adapter partten

---
+++ adapter/adapter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- conding: utf-8 -*- 
+
+class Adaptee:
+    def do_something(self):
+        print('Target to something')
+
+class Adapter:
+
+    def __init__(self, target):
+        if target == None:
+            raise Exception('Adapter', 'Error Target')
+
+        self.adaptee = target
+
+    def do_something(self):
+        self.adaptee.do_something();
+
+if __name__ == "__main__":
+    a = Adapter(Adaptee())
+    a.do_something()
