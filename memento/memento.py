e701e7859205fd32bcd66b8fa403a8604174b3e2
sean <zhxiang.guan@gmail.com>
2016-11-03 00:31:31 +0800

    Create Memento Pattern file

---
+++ memento/memento.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Memento:
+
+    def __init__(self, hp=100, mp=100):
+        self.hp = hp
+        self.mp = mp
+
+
+class Originator:
+
+    def __init__(self):
+        pass
+
+    det set_mento(self, memo = Memento()):
+        pass
+
+class Caretaker:
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
