bae8ffbfa85355b605225328df7eb8740e653b78
sean <zhxiang.guan@gmail.com>
2016-11-03 23:33:47 +0800

    class memento is work. 1. formate output '{}'.formate() 2. toString() : __str__

--- memento/memento.py
+++ memento/memento.py
@@ -7,18 +7,37 @@ class Memento:
         self.hp = hp
         self.mp = mp
 
+    def __str__(self):
+        return '{} {}'.format(self.hp, self.mp)
 
 class Originator:
 
-    def __init__(self):
-        pass
+    def __init__(self, memo = Memento()):
+        self.memento = memo
 
-    det set_mento(self, memo = Memento()):
-        pass
+    def save(self, memo):
+        self.memento = memo
+
+    def load(self):
+        return self.memnto
+
+    def display_status(self):
+        print(self.memento)
 
 class Caretaker:
 
-    def __init__(self):
-        pass
+    def __init__(self, memo = Memento()):
+        self.recovery(memo)
+
+    def recovery(self, memo):
+        self.memento = memo
+
+    def display_status(self):
+        print(self.memento)
 
 if __name__ == "__main__":
+    memo = Memento(30, 20)
+    print(memo)
+
+    admin = Originator()
+    client = Caretaker()
