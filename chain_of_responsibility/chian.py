a38fc3781e5a69f3c143e5fdd624603671e784c7
sean <zhxiang.guan@gmail.com>
2016-11-10 02:32:24 +0800

    Finish Chian of Responsibility Pattern.

--- chain_of_responsibility/chian.py
+++ chain_of_responsibility/chian.py
@@ -2,23 +2,43 @@
 # -*- conding: utf-8 -*- 
 
 class Handler:
-    pass
+
+    def set_successor(self, successor):
+        self.successor = successor
+
+    def handle_request(self, request):
+        pass
 
 class HandlerA(Handler):
-    pass
+    
+    def handle_request(self, request):
+        if request < 5:
+            print('A Handler', request)
+        elif self.successor != None:
+            self.successor.handle_request(request)
 
-class HandlerA(Handler):
-    pass
+class HandlerB(Handler):
+    
+    def handle_request(self, request):
+        if request >= 5 and request < 10:
+            print('B Handler', request)
+        elif self.successor != None:
+            self.successor.handle_request(request)
 
 
+class HandlerC(Handler):
+    
+    def handle_request(self, request):
+        if request:
+            print('C Handler', request)
 
 if __name__ == '__main__':
     handler_a = HandlerA()    
     handler_b = HandlerB()    
     handler_c = HandlerC()
 
-    handler_a.setSuccessor(handler_b)
-    handler_b.setSuccessor(handler_c)
+    handler_a.set_successor(handler_b)
+    handler_b.set_successor(handler_c)
 
     for request in 1, 2, 6, 7, 8, 10 ,20:
-        h1.handleRequest(request)
+        handler_a.handle_request(request)
