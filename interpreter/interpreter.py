9c4b1a7083100698a6d6d858a3594a3333b93db9
sean <zhxiang.guan@gmail.com>
2016-11-13 23:54:50 +0800

    Create interpreter pattern.

---
+++ interpreter/interpreter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- conding: utf-8 -*- 
+
+class TermainalExpresstion:
+    pass
+
+class NontermianalExpresstion:
+    pass
+
+class Context:
+    pass
+
+if __name__ == '__main__':
