97e43ba1e43945721c66468092afcd90081ea653
sean <zhxiang.guan@gmail.com>
2016-11-23 00:20:48 +0800

    Complete heapsort

--- algoriyhms/heapsort.py
+++ algoriyhms/heapsort.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 # -*- conding: utf-8 -*- 
 
-def heapsort(array):
-    for i in range(len(array)-1, -1, -1):
-        heapify(array, i)
-
-def heapify(array, i):
+def heapify(array, i, heap_size):
     largest = i 
     while largest <= len(array):
         first = largest
         left = 2 * first + 1
         right = 2 * first + 2
 
-        if right < len(array) and array[right] > array[largest]:
+        if right < heap_size and array[right] > array[largest]:
             largest = right 
 
-        if left < len(array) and array[left] > array[largest]:
+        if left < heap_size and array[left] > array[largest]:
             largest = left 
 
         if largest is not first:
@@ -24,6 +20,21 @@ def heapify(array, i):
             array[largest] = temp
         else:
             return
+
+def build_max_heap(array):
+    for i in range(len(array) - 1, -1, -1):
+        heapify(array, i, len(array))
+
+def heapsort(array):
+    build_max_heap(array)
+    heap_size = len(array)
+
+    for i in range(len(array) - 1, 0, -1):
+        temp = array[0]
+        array[0] = array[i]
+        array[i] = temp
+        heap_size -= 1
+        heapify(array, 0, heap_size)
 
 if __name__ == '__main__':
     a = [3, 2, 9, 1, 20, 10]
