00d7a72808465f3e1dee5fac46365284dc21436f
sean <zhxiang.guan@gmail.com>
2016-11-30 00:13:48 +0800

    Add red-black tree.

---
+++ data_structure/red_black_tree.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- conding: utf-8 -*- 
+
+class RedBlackTree:
+    
+    def set_parent(self, parent):
+        this.parent = parent
+
+    def set_left_node(self, left):
+        this.left = left
+
+    def set_right_node(self, right):
+        this.right = right
+
+    def set_color(self, color):
+        this.color = color
+
+    def set_key(self, key):
+        this.key = key
